01c1d249a2904fce7814c2d4be15c2422424eabc
Liu Zechu <liuzechu2013@gmail.com>
2020-09-06 09:58:15 +0800

    change server port to dynamic

--- app.js
+++ app.js
@@ -8,6 +8,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
-});
+var server = app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
